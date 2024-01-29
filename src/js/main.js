6d700057dafecbd96687df0fe005618da48d73df
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-01-29 21:12:27 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import '/src/scss/style.scss';
+
+const burgerMenu = document.querySelector('.header__burger');
+const menuBody = document.querySelector('.menu');
+
+if(burgerMenu) {
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        menuBody.classList.toggle('open');
+    })
+}
